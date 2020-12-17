f6da5dc1f3baea8df3ce976c935a5a4bbebdd7a1
Pete DeGraw <petedegraw [at] icloud.com>
2020-12-17 00:26:57 -0500

    adds domains

--- index.js
+++ index.js
@@ -1,92 +1,147 @@
+const dotenv = require('dotenv');
+var colors = require('colors');
 const { http, https } = require('follow-redirects');
 const xlsxFile = require('read-excel-file/node');
 const fs = require('fs');
 const PDFDocument = require('./utils/pdfkit-tables');
+const { exec } = require('child_process');
 
+dotenv.config();
+
+
+if (!process.env.domains) {
+    console.error('Oops, first create a .env file and add domains=https://mywebsite.com,https://mywebsite.com.jp'.red);
+    return;
+}
+
+let domains = process.env.domains.split(',');
+let d = new Date();
 // Create a document
 const doc = new PDFDocument({width: 1800, margin: 50});
-    
 
-var d = new Date();
-
-// Pipe its output somewhere, like to a file or HTTP response
-// See below for browser usage
-doc.pipe(fs.createWriteStream('./data/reports/dmo-863--prod.pdf'));
+doc.pipe(fs.createWriteStream(process.env.dir_path + '/data/reports/dmo-863--prod.pdf'));
 
 doc.font('Helvetica')
     .fontSize(14)
-    .text('DMO-863 Redirect Tests -- Production')
+    .text('DMO-863 Redirect Tests — Production')
     .moveDown(.5);
 
 doc.fontSize(10)
     .text('Tested on ' + d.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' }) + ' by Pete DeGraw')
     .moveDown(.5);
 
-let fails = 0;
-
 let table0 = {
     headers: [
-        'From',
-        // 'To',
-        'Actual',
-        'Result'],
+        'Expected',
+        'Observed',
+        'Status'],
     rows: []
 };
 
-let redirectReport = [];
+// let redirectReport = [];
+let fails = 0;
 let count = 0;
-xlsxFile('./data/redirects/dmo-863.xlsx').then((rows) => {
-    rows.forEach((col, index)=>{
-        if (index > 0) {
-            let from = 'https://www.equinix.co.jp' + col[0];
-            let to = 'https://www.equinix.co.jp' + col[1];
-            https.get(from, response => {
-                let url = response.responseUrl;
-                let result = url === to;
-                let report = [
-                    from,
-                    // to,
-                    url,
-                    result ? 'PASS' : 'FAIL'
-                ];
-                // redirectReport.push(report);
-                if (result === false) {
-                    fails++;
-                }
-                table0.rows.push(report);
-                // console.table(table0.rows)
-                count++;
+let urlCount = 0;
+let rowsCount = 0;
 
-                if (count === rows.length - 1) {
-                    // console.table(redirectReport);
-                    // console.log(table0)
-                    // Finalize PDF file
-                    doc.fontSize(12)
-                        .font('Helvetica-Bold')
-                        .text(fails > 0 ? 'Result: FAIL' : 'Result: PASS')
-                        .moveDown(0.5);
-                    if (result === true) {
-                        doc.image('media/qcpassed.png', 520, 25, {
-                            fit: [50, 50],
-                            align: 'right',
-                            valign: 'top'
-                        });
-                    } else {
-                        doc.image('media/fail.jpg', 520, 25, {
-                            fit: [50, 50],
-                            align: 'right',
-                            valign: 'top'
-                        });
-                    }
-                    doc.table(table0, {
-                        prepareHeader: () => doc.font('Helvetica-Bold'),
-                        prepareRow: (row, i) => doc.font('Helvetica').fontSize(8)
-                    });
-                    doc.end();
-                }
-            }).on('error', err => {
-                console.error(err);
+function finalizePdf(domain) {
+    console.log('[✔]'.green.bold, '-'.grey, 'testing complete'.magenta, '-'.grey, domain);
+    if (table0.rows.length === domains.length * (rowsCount - 1)) {
+        console.log('finalizing pdf'.grey);
+        // console.table(redirectReport);
+        // Finalize PDF file
+        doc.fontSize(12)
+            .font('Helvetica-Bold')
+            .text(fails > 0 ? 'Ticket Status: FAILED' : 'Result: PASSED')
+            .moveDown(0.5);
+        if (fails === 0) {
+            doc.image('media/qcpassed.png', 520, 25, {
+                fit: [50, 50],
+                align: 'right',
+                valign: 'top'
+            });
+        } else {
+            doc.image('media/fail.jpg', 520, 25, {
+                fit: [50, 50],
+                align: 'right',
+                valign: 'top'
             });
         }
+        doc.table(table0, {
+            prepareHeader: () => doc.font('Helvetica-Bold'),
+            prepareRow: (row, i) => doc.font('Helvetica').fontSize(8)
+        });
+        doc.end();
+        exec('open ' + process.env.dir_path + '/data/reports/dmo-863--prod.pdf', (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            // console.log(`stdout: ${stdout}`);
+        });
+    }
+}
+
+console.log('testing redirects'.gray);
+
+domains.forEach(domain => {
+    // console.log('...'.green + domain.green);
+
+    xlsxFile(process.env.dir_path + '/data/redirects/dmo-863.xlsx').then((rows) => {
+        rowsCount = rows.length;
+
+        rows.forEach((col, index)=>{
+
+            // skip the "to" and "from" row
+            if (index > 0) {
+                
+                    let from = domain + col[0];
+                    let to = domain + col[1];
+
+                    https.get(from, response => {
+                        let url = response.responseUrl;
+                        let result = url === to;
+                        let report = [
+                            to,
+                            url,
+                            result ? 'PASSED' : 'FAILED'
+                        ];
+                        // redirectReport.push(report);
+                        if (result === false) {
+                            fails++;
+                        }
+                        
+                        // publish the results
+                        table0.rows.push(report);
+                        urlCount++;
+
+                        // checks
+                        // console.log(count, urlCount, rows.length - 1);
+                        // console.log('domains:', count, domains.length - 1);
+                        // console.log('build:', table0.rows.length, domains.length * (rowsCount - 1))
+                        // console.log('fails', fails);
+
+                        // conclude all rows
+                        if (urlCount === rows.length - 1) {
+                            // conclude domain
+                            count++;
+                            // reset urlCounter
+                            urlCount = 0;
+                            // finalize PDF
+                            finalizePdf(domain);
+                        }
+                        
+                    }).on('error', err => {
+                        console.error(err);
+                    });
+
+            } // end skipping "to" and "from" row
+
+        });
     });
+
 });
