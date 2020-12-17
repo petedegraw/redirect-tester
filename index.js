47c70de8bdd90095efca602e6d6af4ff70de29c6
Pete DeGraw <pdegraw@equinix.com>
2020-12-16 20:12:07 -0500

    adds pdf output

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const { http, https } = require('follow-redirects');
 const xlsxFile = require('read-excel-file/node');
+const fs = require('fs');
+const PDFDocument = require('./utils/pdfkit-tables');
+
+// Create a document
+const doc = new PDFDocument;
+    
+// Pipe its output somewhere, like to a file or HTTP response
+// See below for browser usage
+doc.pipe(fs.createWriteStream('./data/reports/dmo-863.pdf'));
+
+doc.font('Helvetica')
+    .fontSize(14)
+    .text('DMO-863 Redirect Tests -- Production')
+    .moveDown(.5);
+
+let fails = 0;
+
+let table0 = {
+    headers: ['From', 'To', 'Actual', 'Result'],
+    rows: []
+};
 
 let redirectReport = [];
 let count = 0;
@@ -11,11 +32,29 @@ xlsxFile('./data/redirects/dmo-863.xlsx').then((rows) => {
             http.get(from, response => {
                 let url = response.responseUrl;
                 let result = url === to;
-                let report = {from, to, url, result};
-                redirectReport.push(report);
+                let report = [from, to, url, result ? 'PASS' : 'FAIL'];
+                // redirectReport.push(report);
+                if (result === false) {
+                    fails++;
+                }
+                table0.rows.push(report);
+                // console.table(table0.rows)
                 count++;
+
                 if (count === rows.length - 1) {
-                    console.table(redirectReport);
+                    // console.table(redirectReport);
+                    // console.log(table0)
+                    // Finalize PDF file
+                    doc.fontSize(12)
+                        .font('Helvetica-Bold')
+                        // .fillColor(result ? 'green' : 'red')
+                        .text(fails > 0 ? 'FAIL' : 'PASS')
+                        .moveDown(0.5);
+                    doc.table(table0, {
+                        prepareHeader: () => doc.font('Helvetica-Bold'),
+                        prepareRow: (row, i) => doc.font('Helvetica').fontSize(8)
+                    });
+                    doc.end();
                 }
             }).on('error', err => {
                 console.error(err);
